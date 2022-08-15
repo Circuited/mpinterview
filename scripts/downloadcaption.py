2a89b50ef8944b96ae2e2fd47a7b339ba5b2ffac
Circuited <paaras12@gmail.com>
2022-08-16 01:12:57 +0530

    updated code to write transcript, transcript ouput saved

--- scripts/downloadcaption.py
+++ scripts/downloadcaption.py
@@ -1,11 +1,25 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import JSONFormatter
+import json
 
-transcript = YouTubeTranscriptApi.get_transcript("qBdieWfouZM")
+user_input = input("Enter the path of your file: ")
 
-formatter = JSONFormatter()
+with open(user_input) as file:
+	data = json.load(file)
 
-text_formatted = formatter.format_transcript(transcript)
+##transcript = YouTubeTranscriptApi.get_transcript("qBdieWfouZM")
+##
+##formatter = JSONFormatter()
+##
+##text_formatted = formatter.format_transcript(transcript)
+##
+##with open('20220714.json', 'w', encoding='utf-8') as JSON_file:
+##    JSON_file.write(text_formatted)
 
-with open('20220714.json', 'w', encoding='utf-8') as JSON_file:
-    JSON_file.write(text_formatted)
+for indexNumber in range(36,55):
+    transcript = YouTubeTranscriptApi.get_transcript(data[str(indexNumber)]['URL'][-11:])
+    formatter = JSONFormatter()
+    text_formatted = formatter.format_transcript(transcript)
+    fileName = data[str(indexNumber)]['Upload data'][:10]+'.json'
+    with open(fileName, 'w', encoding='utf-8') as JSON_file:
+        JSON_file.write(text_formatted)
