31b635df4b9faa63ef99947c6475ff19859575b7
Circuited <paaras12@gmail.com>
2022-08-15 22:56:26 +0530

    video list updated and added

--- scripts/videolistonchannel.py
+++ scripts/videolistonchannel.py
@@ -1,16 +1,16 @@
 from pytube import Channel
 from pytube import YouTube
 import json
-c = Channel('https://www.youtube.com/user/dakshanafoundation/videos')
+c = Channel('https://www.youtube.com/user/mohnishpabrai/videos')
 mpdataset = {}
 i = 0
 
-for url in c.videos[:30]:
+for url in c.videos[:55]:
     mpdataset[i]={"URL": c.video_urls[i], "Title": url.title, "Upload data":url.publish_date}
     i = i+1
 
 final = json.dumps(mpdataset, indent=2, default=str)
 
-with open('mpdatasetv1.json', 'w', encoding='utf-8') as JSON_file:
+with open('mpdataset.json', 'w', encoding='utf-8') as JSON_file:
     JSON_file.write(final)
 
